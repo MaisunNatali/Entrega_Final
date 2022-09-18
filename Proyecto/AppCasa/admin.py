6af4d1e687a7509235f853fec428b763bf39afa3
Maisun Natali AKil <maisunakil@gmail.com>
2022-09-18 14:49:58 -0300

    Se incluyen avatares a usuarios

--- Proyecto/AppCasa/admin.py
+++ Proyecto/AppCasa/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from AppCasa.models import Casas, Estudiantes,Profesores
+from AppCasa.models import Casas, Estudiantes,Profesores,Avatar
 # Register your models here.
 admin.site.register(Casas)
 admin.site.register(Estudiantes)
 admin.site.register(Profesores)
+admin.site.register(Avatar)
